8957896422f98042e6091a714e2419c67b0d3755
Anthony Song <anthony.b.song@gmail.com>
2024-12-25 19:36:30 -0500

    Created AST.h and a empty AST.cpp

--- include/AST.h
+++ include/AST.h
@@ -1,10 +1,45 @@
 #pragma once
-#include <map>
 
-class parser {
+#include <iostream>
+#include <vector>
+
+// Abstract class for a generic tree node
+template <typename T>
+class AbstractTreeNode {
 public:
+    virtual ~AbstractTreeNode() = default;
 
-private:
+    // Get the value stored in the node
+    virtual T getValue() const = 0;
 
+    // Set the value of the node
+    virtual void setValue(const T& value) = 0;
 
+    // Add a child node
+    virtual void addChild(AbstractTreeNode<T>* child) = 0;
+
+    // Get all child nodes
+    virtual const std::vector<AbstractTreeNode<T>*>& getChildren() const = 0;
+
+    // Remove a child node
+    virtual bool removeChild(AbstractTreeNode<T>* child) = 0;
+};
+
+// Abstract class for a generic tree
+template <typename T>
+class AbstractTree {
+public:
+    virtual ~AbstractTree() = default;
+
+    // Get the root node of the tree
+    virtual AbstractTreeNode<T>* getRoot() const = 0;
+
+    // Set the root node of the tree
+    virtual void setRoot(AbstractTreeNode<T>* root) = 0;
+
+    // Traverse the tree (example traversal type to be implemented)
+    virtual void traverse(std::ostream& os = std::cout) const = 0;
+
+    // Clear the tree
+    virtual void clear() = 0;
 };
