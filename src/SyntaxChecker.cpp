a54ee48aa8915ad97e926e63a92b7e6953e93ece
Gavin Chen <gavinchen9616@gmail.com>
2025-01-01 14:54:08 -0500

    update

--- src/SyntaxChecker.cpp
+++ src/SyntaxChecker.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <memory>
+#include "AST.h"
+#include "TreeNode.hpp"
+#include "SyntaxTree.hpp" 
 
+//SyntaxChecker class
+class SyntaxChecker {
+    public:
+    //Constructor
+    SyntaxChecker() = default;
+
+    //Check for tree being syntactically correct
+    bool checkSyntax(const AbstractTree<std::string>& tree) const {
+        //Check root
+        AbstractTreeNode<std::string>* root = tree.getRoot();
+        if (!root) {
+            std::cerr << "Warning! The tree is empty (no root node).\n";
+            return false;
+        }
+        return validNode(root);
+    }
+
+    private:
+    //Function to validate nodes
+    bool validNode(AbstractTreeNode<std::string>* node) const {
+        //Base Case: If node is nullptr, return true
+        return;
+    }
+}
